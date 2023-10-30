ac508e593e75341e35a2f278ce1ade5e33c78ec0
ahmedaliyakhalil <142332632+ahmedaliyakhalil@users.noreply.github.com>
2023-10-30 23:10:05 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,185 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img :"images/biryani/biryani1.jpg",
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: "images/biryani/biryani1.jpg",
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'images/biryani/biryani3.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/biryani4.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 10,
+    img: 'images/biryani/biryani5.jpg',
+    quantity: 1
+},
+{ity: 1
+},
+
+{
+    id: 6,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: 'images/ckicken/chicken1.webp',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: 'images/ckicken/chicken3.jpeg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/paneer/panner1.jpg',
+    quantity: 1
+
+},
+{
+    id: 9,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 10,
+    img: 'images/paneer/panner2.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/paneer/panner3.jpg',
+    quantity: 1
+
+},
+
+{
+    id: 11,
+    name: 'Navratan Korma',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 8,
+    img: 'images/vegetable/vegitable1.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Veg Jalfrezi',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/vegetable/vegitable2.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 5,
+    img: 'images/vegetable/vegitable3.jpg',
+    quantity: 1
+},
+
+{
+    id: 14,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chinese/chinese1.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: 'images/chinese/chinese2.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Chili Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: 'images/chinese/chinese3.jpg',
+    quantity: 1
+},
+
+{
+    id: 17,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 18,
+    img: 'images/south indian/download.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 4.3,
+    price: 20,
+    img: 'images/south indian/south2.jpg',
+    quantity: 1
+},
+{
+    id: 19,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 12,
+    img: 'images/south indian/south3.jpg',
+    quantity: 1
+},
+
+   
+]
+
+export {foodItem};
